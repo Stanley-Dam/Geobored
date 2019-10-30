cc35adee6284735ca6dd79864f6e69a5012022e6
LiableDuke <43440836+LiableDuke@users.noreply.github.com>
2019-10-30 22:08:30 +0100

    added my part of the game

---
+++ GeoboredMultiplayer/Assets/Tim/Scripts/Player/Shooting.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shooting : MonoBehaviour
+{
+    #region Variables
+    private GameObject pistolBullet;
+    private Transform bulletSpawner;
+    #endregion
+
+    private void Start()
+    {
+        pistolBullet = (GameObject)Resources.Load("Bullets/Prefabs/PistolBullet");
+        bulletSpawner = this.transform.Find("BulletSpawner");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+            Fire();
+    }
+
+    private void Fire()
+    {
+        Instantiate(pistolBullet, bulletSpawner.position, bulletSpawner.rotation);
+    }
+}
