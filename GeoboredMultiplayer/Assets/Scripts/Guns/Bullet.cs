89df8019703777eb1c263045aa8451c1058a1c4a
Stanley-Dam <ditisstanley@gmail.com>
2019-10-31 11:52:55 +0100

    Files added
    Done

---
+++ GeoboredMultiplayer/Assets/Scripts/Guns/Bullet.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    #region Variables
+    [SerializeField] private float bulletSpeed;
+    [SerializeField] private int damage;
+    private GameObject hitFX;
+    private AudioSource audioS;
+    #endregion
+
+    private void Start()
+    {
+        //plays sound fx with random pitch
+        hitFX = (GameObject)Resources.Load("Bullets/FX/hitFX");
+        audioS = this.GetComponent<AudioSource>();
+        float pitch = Random.Range(0.9f, 1.1f);
+
+        audioS.pitch = pitch;
+        audioS.Play(0);
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        this.transform.position += transform.right * bulletSpeed * Time.deltaTime;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        bulletSpeed = 0;
+
+        ContactPoint2D contact = collision.GetContact(0);
+        Quaternion rot = Quaternion.FromToRotation(Vector3.up, contact.normal);
+        Vector3 pos = contact.point;
+
+        if (hitFX)
+        {
+            var hitFXIns = Instantiate(hitFX, pos, rot);
+            try
+            {
+                Color color = collision.transform.GetComponent<SpriteRenderer>().color;
+                hitFXIns.GetComponent<PlaySoundParticel>().SetColor = color;
+            }
+            catch
+            {
+                hitFXIns.GetComponent<PlaySoundParticel>().SetColor = Color.white;
+            }
+            
+        }
+        if (collision.gameObject.layer == 9)
+            collision.gameObject.SendMessage("TakeDamage", damage);
+        Destroy(this.gameObject);
+    }
+}
