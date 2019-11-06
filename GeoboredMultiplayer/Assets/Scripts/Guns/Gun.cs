c006a424dac2c618d71e0e7a3d87a8d8a8d7622e
LiableDuke <43440836+LiableDuke@users.noreply.github.com>
2019-11-06 15:59:14 +0100

    fixed errors that happend during the merge

---
+++ GeoboredMultiplayer/Assets/Scripts/Guns/Gun.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    protected GameObject bullet;
+    protected int damage;
+    protected Transform bulletSpawner;
+    protected float spread = 4f;
+    protected bool canShoot = true;
+    protected float coolDownTime = 0.01f;
+
+    protected void Awake()
+    {
+        bulletSpawner = this.transform.Find("BulletSpawner");
+    }
+
+    // Update is called once per frame
+    protected IEnumerator Fire(int bulletAmount)
+    {
+        for (int i = 0; i < bulletAmount; i++)
+        {
+            bulletSpawner.localRotation = Quaternion.AngleAxis(Random.Range(-spread, spread), bulletSpawner.forward);
+            var bulletIns = Instantiate(bullet, bulletSpawner.position, bulletSpawner.rotation);
+            bulletIns.GetComponent<Bullet>().BulletOwner = this.gameObject;
+            bulletSpawner.localRotation = Quaternion.AngleAxis(0, bulletSpawner.forward);
+        }
+        canShoot = false;
+        StartCoroutine(CoolDown());
+        yield return null;
+    }
+
+    private IEnumerator CoolDown()
+    {
+        yield return new WaitForSeconds(coolDownTime);
+        canShoot = true;
+    }
+}
---
+++ GeoboredMultiplayer/Assets/Scripts/Guns/ShotGun.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotGun : Gun
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+        spread = 7f;
+        bullet = (GameObject)Resources.Load("Bullets/Prefabs/ShotGunShell");
+    }
+
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && canShoot)
+        {
+            StartCoroutine(Fire(5));
+        }   
+    }
+}
